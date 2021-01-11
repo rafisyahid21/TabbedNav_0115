5adad96b142c62458643d3d99ce333ff04275ae7
rafisyahid21 <Muhammad.2107@students.amikom.ac.id>
2021-01-12 06:33:29 +0700

    TabbedNav_0115

---
+++ app/src/main/java/com/example/tabbednav_0115/Movie.java
@@ -0,0 +1,33 @@
+package com.example.tabbednav_0115;
+
+
+import java.util.List;
+
+public class Movie {
+    private String title;
+    private String year;
+
+    public Movie(String title, String year) {
+        this.title = title;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
+
+
+
